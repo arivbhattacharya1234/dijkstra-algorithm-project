383db30350af3539e94458bc59183cdf5ff1c74f
Ariv Bhattacharya <ariv.bhattacharya@gmail.com>
2026-05-27 12:00:25 +0530

    Updated Dijkstra algorithm

--- Djikstra_code.py
+++ Djikstra_code.py
@@ -1,5 +1,7 @@
 import networkx as nx
 import matplotlib.pyplot as plt
+import random
+from itertools import permutations
 
 G = nx.Graph()
 
@@ -26,51 +28,87 @@ for u, v, w in edges:
 
     G.add_edge(u, v, weight=total_weight, distance=distance, traffic=traffic)
 
+
+for u, v, data in G.edges(data=True):
+      dynamic_traffic = random.randint(1, 5)
+
+data['traffic'] = dynamic_traffic
+data['weight'] = data['distance'] + dynamic_traffic
+
 print("Available locations:", list(G.nodes()))
 
 start = input("Enter start location: ")
 destinations_input = input("Enter destinations (comma separated): ")
 destinations = [d.strip() for d in destinations_input.split(",")]
 
-current = start
-full_path = []
-total_cost = 0
-total_distance = 0
-all_path_edges = []
+best_order = None
+best_cost = float('inf')
+best_full_path = []
+best_path_edges = []
+best_total_distance = 0
 
-while destinations:
-    shortest = None
-    shortest_path = None
-    min_cost = float('inf')
+# Try all possible delivery orders
+for order in permutations(destinations):
 
-    for dest in destinations:
-        path = nx.shortest_path(G, source=current, target=dest, weight='weight')
-        cost = nx.shortest_path_length(G, source=current, target=dest, weight='weight')
+    current = start
+    total_cost_temp = 0
+    total_distance_temp = 0
+    full_path_temp = []
+    path_edges_temp = []
 
-        if cost < min_cost:
-            min_cost = cost
-            shortest = dest
-            shortest_path = path
+    possible = True
 
-    full_path.extend(shortest_path[:-1])
-    total_cost += min_cost
+    for dest in order:
 
-    path_edges = list(zip(shortest_path, shortest_path[1:]))
-    all_path_edges.extend(path_edges)
+        try:
+            path = nx.shortest_path(
+                G,
+                source=current,
+                target=dest,
+                weight='weight'
+            )
 
-    total_distance += sum(G[u][v]["distance"] for u, v in path_edges)
+            cost = nx.shortest_path_length(
+                G,
+                source=current,
+                target=dest,
+                weight='weight'
+            )
 
-    current = shortest
-    destinations.remove(shortest)
+        except:
+            possible = False
+            break
 
-full_path.append(current)
+        full_path_temp.extend(path[:-1])
 
-print("Full Delivery Path:", full_path)
-print("Total Cost:", total_cost)
-print("Total Distance:", total_distance, "km")
+        total_cost_temp += cost
+
+        path_edges = list(zip(path, path[1:]))
+        path_edges_temp.extend(path_edges)
+
+        total_distance_temp += sum(
+            G[u][v]["distance"]
+            for u, v in path_edges
+        )
+
+        current = dest
+
+    full_path_temp.append(current)
+
+    if possible and total_cost_temp < best_cost:
+        best_cost = total_cost_temp
+        best_order = order
+        best_full_path = full_path_temp
+        best_path_edges = path_edges_temp
+        best_total_distance = total_distance_temp
+
+print("Best Delivery Order:", best_order)
+print("Optimized Delivery Path:", best_full_path)
+print("Minimum Total Cost:", best_cost)
+print("Total Distance:", best_total_distance, "km")
 
 speed = 40
-eta = total_distance / speed
+eta = best_total_distance / speed
 
 print("Estimated Time:", round(eta, 2), "hours")
 
@@ -78,7 +116,7 @@ pos = nx.spring_layout(G)
 
 nx.draw(G, pos, with_labels=True, node_color='lightblue', node_size=2000)
 
-nx.draw_networkx_edges(G, pos, edgelist=all_path_edges, edge_color='red', width=3)
+nx.draw_networkx_edges(G, pos, edgelist=best_path_edges, edge_color='red', width=3)
 
 edge_labels = {}
 for u, v, data in G.edges(data=True):
